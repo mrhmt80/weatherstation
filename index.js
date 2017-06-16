7ab0df5fb984c66d01eacc3ad910bdb9d784694a
Guillermo López Leal <guillermo@tokbox.com>
2017-06-16 13:35:39 +0200

    Port from env

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.get('/', (req, res) => {
   })
 })
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Hello, rubicon http://localhost:${PORT}`)
 })
